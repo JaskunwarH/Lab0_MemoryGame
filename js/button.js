abe8d52833e8d4e24a8c9e92203c9cf1231d7ea7
JaskunwarH <156830389+JaskunwarH@users.noreply.github.com>
2025-09-06 15:23:58 -0700

    Initial commit

---
+++ js/button.js
@@ -0,0 +1,16 @@
+class Button 
+{
+    int number;
+    string hexcodeforcolor;
+
+    Button(number, hexcodeforcolor)
+    {
+        this.number = number;
+        this.hexcodeforcolor = hexcodeforcolor;
+    }
+
+    void click ()
+    {
+        return number
+    }
+}
